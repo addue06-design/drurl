e5902b7fb16d00fbe978ed31fcc4ffc94fb81bb3
addue06-design <addue.06@gmail.com>
2025-12-22 18:51:33 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,7 +6,7 @@ import time
 from playwright.async_api import async_playwright
 
 # --- 設定區域 ---
-TARGET_ID = "5597942" 
+TARGET_ID = "5591004" 
 BASE_DOMAIN = "dramaq.xyz"
 OUTPUT_FILE = "all_episodes_results.txt"
 # ------------------
